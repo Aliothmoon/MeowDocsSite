2a9073f2d5c00f43a4cf46b43e62dcb3fb29843e
Aliothmoon <aliothmoon@outlook.com>
2026-09-04 21:24:00 +0800

    Set site URL to https://docs.maameow.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlightThemeNova from 'starlight-theme-nova';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://docs.maameow.com',
 	integrations: [
 		starlight({
 			plugins: [
